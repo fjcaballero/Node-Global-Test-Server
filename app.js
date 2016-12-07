aeef51a34d59840136e6ce145539f90380742e2a
Javier Caballero <javier.caballero.abenza@gmail.com>
2016-12-07 13:22:55 +0100

    update: testing telegram

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.use(express.static('bower_components'));
 
 //WebComponents
 app.use(express.static('../WebComponents'));
+app.use(express.static('../Versionado'));
+app.use(express.static('../WebComponents2.0'));
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
@@ -25,6 +27,17 @@ app.get('/', function (req, res) {
 app.get('/config', function (req, res) {
 	var json = require('./config.json');
   res.send(json);
+});
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'yolo') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
 });
 
 var server = https.createServer({
